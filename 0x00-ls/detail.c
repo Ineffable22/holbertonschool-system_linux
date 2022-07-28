8cb6c717a91a7737c1da00f99b09019a30a66c70
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-07-28 04:28:39 -0500

    Feat(Betty(C99)): Style was fixed detail.c

--- 0x00-ls/detail.c
+++ 0x00-ls/detail.c
@@ -4,15 +4,16 @@
  * more_detail - Derives information to other functions to
  * get print details from ls
  * @head: pointer to struct Sort with singly linked list
- *
+ * @op: pointer to struc Option with printing options
  * Return: Nothing
  */
-void more_detail(sort *head)
+void more_detail(sort *head, option *op)
 {
 	char *time;
-        struct passwd *usr;
-        struct group *grp;
+	struct passwd *usr;
+	struct group *grp;
 	int i = 0, j = 0;
+
 	types(head->st_mode);
 	rights(head->st_mode);
 	printf(" %ld", head->st_nlink);
@@ -21,7 +22,7 @@ void more_detail(sort *head)
 	printf(" %s %s ", usr->pw_name, grp->gr_name);
 	time = ctime(&(head->st_time));
 	i = count_digit(head->st_size);
-	j = count_digit(size_file);
+	j = count_digit(op->size_file);
 	while (j != i)
 	{
 		putchar(32);
@@ -39,15 +40,15 @@ void more_detail(sort *head)
  */
 void rights(mode_t mode)
 {
-        printf((mode & S_IRUSR) ? "r" : "-");
-        printf((mode & S_IWUSR) ? "w" : "-");
-        printf((mode & S_IXUSR) ? "x" : "-");
-        printf((mode & S_IRGRP) ? "r" : "-");
-        printf((mode & S_IWGRP) ? "w" : "-");
-        printf((mode & S_IXGRP) ? "x" : "-");
-        printf((mode & S_IROTH) ? "r" : "-");
-        printf((mode & S_IWOTH) ? "w" : "-");
-        printf((mode & S_IXOTH) ? "x" : "-");
+	printf((mode & S_IRUSR) ? "r" : "-");
+	printf((mode & S_IWUSR) ? "w" : "-");
+	printf((mode & S_IXUSR) ? "x" : "-");
+	printf((mode & S_IRGRP) ? "r" : "-");
+	printf((mode & S_IWGRP) ? "w" : "-");
+	printf((mode & S_IXGRP) ? "x" : "-");
+	printf((mode & S_IROTH) ? "r" : "-");
+	printf((mode & S_IWOTH) ? "w" : "-");
+	printf((mode & S_IXOTH) ? "x" : "-");
 }
 
 /**
@@ -58,50 +59,50 @@ void rights(mode_t mode)
  */
 void types(mode_t mode)
 {
-        switch (mode & S_IFMT)
-        {
-        case S_IFREG:
-                printf("-");
-                break;
-        case S_IFDIR:
-                printf("d");
-                break;
-        case S_IFCHR:
-                printf("c");
-                break;
-        case S_IFBLK:
-                printf("b");
-                break;
-        case S_IFIFO:
-                printf("p");
-                break;
-        case S_IFLNK:
-                printf("l");
-                break;
-        case S_IFSOCK:
-                printf("s");
-                break;
-        }
+	switch (mode & S_IFMT)
+	{
+	case S_IFREG:
+		printf("-");
+		break;
+	case S_IFDIR:
+		printf("d");
+		break;
+	case S_IFCHR:
+		printf("c");
+		break;
+	case S_IFBLK:
+		printf("b");
+		break;
+	case S_IFIFO:
+		printf("p");
+		break;
+	case S_IFLNK:
+		printf("l");
+		break;
+	case S_IFSOCK:
+		printf("s");
+		break;
+	}
 }
 
 /**
- * time_format - Prints the time from the file in a 
- * required format 
- * @mode: Variable with time of last modification
+ * time_format - Prints the time from the file in a
+ * required format
+ * @time: Variable with time of last modification
  *
  * Return: Nothing
  */
 void time_format(char *time)
 {
-        int i = 4, colon = 0;
+	int i = 4, colon = 0;
 
-        for (; colon < 2; i++)
-        {
-                putchar(time[i]);
-                if (time[i + 1] == ':')
-                        colon++;
-        }
-        putchar(32);
+	for (; colon < 2; i++)
+	{
+		putchar(time[i]);
+		if (time[i + 1] == ':')
+			colon++;
+	}
+	putchar(32);
 }
 
 /**
@@ -121,7 +122,7 @@ int type_hidden(int h, sort *head)
 	else if (h == 2)
 	{
 		if (((head->r)[0] == '.' && (head->r)[1] == '\0')
-		    || ((head->r)[0] == '.' && (head->r)[1] == '.'))
+			|| ((head->r)[0] == '.' && (head->r)[1] == '.'))
 			return (0);
 	}
 	return (1);
