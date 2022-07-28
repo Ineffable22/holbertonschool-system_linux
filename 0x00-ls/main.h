43dabb0206032a7f2cc3a125e59e9b05a4e1a7b1
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-07-28 04:26:33 -0500

    Feat(Documentation): Structures and functions were documented main.h

--- 0x00-ls/main.h
+++ 0x00-ls/main.h
@@ -19,6 +19,18 @@
 extern int errno;
 
 /* structure */
+
+/**
+ * struct Sort - struct Sort with singly linked list
+ * @r: File name
+ * @st_mode: permission value
+ * @st_time: time value
+ * @st_size: size value
+ * @st_uid: user value
+ * @st_gid: group value
+ * @st_nlink: hardlink value
+ * @next: Pointer to the next node in the struct Sort
+ */
 typedef struct Sort
 {
 	char *r;
@@ -31,39 +43,72 @@ typedef struct Sort
 	struct Sort *next;
 } sort;
 
+/**
+ * struct Save - struct Save with two singly linked list
+ * @h: Pointer to struct Sort with singly linked list
+ * @file: File name
+ * @next: Pointer to the next node in the struct Save
+ */
 typedef struct Save
 {
 	struct Sort *h;
-	char* file;
+	char *file;
 	struct Save *next;
 } save;
 
+/**
+ * struct Option - Printing options
+ * @hidden: 0 not visible / 1 visible / 2 visible minus . and ..
+ * @order: 0 default / 1 reverse / 2 time / 3 size
+ * @detail: 0 column / 1 row / 2 detail
+ * @recursive: 0 normal / 1 recursive
+ * @size_file: Maximum size of a file in the folder
+ */
 typedef struct Option
 {
-	int hidden; /* 0 no visibles / 1 visibles / 2 visibles menos . .. */
-	int order; /* 0 ningun orden / 1 reverse / 2 time / 3 size */
-	int detail; /* 0 column / 1 row  / 2 detail */
-	int recursive; /* 0 normal / 1 recursive */
+	int hidden;
+	int order;
+	int detail;
+	int recursive;
+	long int size_file;
 } option;
 
 /* prototypes */
+/* ----- main.c ----- */
 void validate(char *flags);
 int validate_weight(char **argv, char *flags, int count);
 void ls(char **av, int c, option *op);
-void more_detail(sort *head);
+DIR *open_case(DIR *dir, char *av);
+
+/* ----- create.c ----- */
+save *create_big_list(save *safe, char *av, DIR *dir, option *op);
+sort *create_list(char *av, sort *head, DIR *dir, option *op);
+sort *add_node(sort *h, char *name, char *av, option *op);
+
+/* ----- detail.c ----- */
+void more_detail(sort *head, option *op);
 void types(mode_t mode);
 void rights(mode_t mode);
 void time_format(char *time);
-sort* create_list(char *av, sort *head, DIR *dir);
-sort* reverse_sort(sort *head);
-sort* time_sort(sort *head);
-sort* size_sort(sort *head);
 int type_hidden(int h, sort *head);
-sort* add_node(sort *h, char *name, char *av);
-DIR* open_case(DIR *dir, char *av);
+
+/* ----- free.c ----- */
+void free_big_list(save *safe);
 void free_list(sort *head);
+
+/* ----- tools.c ----- */
 int count_digit(long int num);
-void printer(save *safe, char* dt, int end, int c, option *op);
-save* create_big_list(save *safe, char *av, DIR *dir, option *op);
+void printer(save *safe, char *dt, int end, int c, option *op);
+
+/* ----- sort.c ----- */
+sort *sorting(sort *tmp, sort *node, option *op);
+sort *order_2(sort *node, sort *tmp, sort *head);
+sort *order_3(sort *node, sort *tmp, sort *head);
+
+/*
+ * sort* time_sort(sort *head);
+ * sort* size_sort(sort *head);
+ * sort* reverse_sort(sort *head);
+ */
 
 #endif
