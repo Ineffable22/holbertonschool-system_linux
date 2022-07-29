6398cf2673eebe02682c8ee1d640545f7e20d085
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-07-28 20:34:53 -0500

    Feat(Betty(C99)): style was update tools.c

--- 0x00-ls/tools.c
+++ 0x00-ls/tools.c
@@ -63,8 +63,7 @@ void printer(save *safe, char *dt, int end, int c, option *op)
 /**
  * _realloc - reallocates a memory block using malloc and free
  * @ptr: pointer to the memory previsouly allocated by malloc
- * @old_size: size of the allocated memory for ptr
- * @new_size: new size of the new memory block
+ * @size: new size of the new memory block
  *
  * Return: pointer to the newly allocated memory block
  */
