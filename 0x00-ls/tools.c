bfed63de37d623d3730783264aeb3a8460961704
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-07-30 01:54:01 -0500

    Feat(fix option): files inside directories with options are fixed tools.c

--- 0x00-ls/tools.c
+++ 0x00-ls/tools.c
@@ -18,10 +18,10 @@ int count_digit(long int num)
 /**
  * printer - Prints the LS output with the chosen option
  * @safe: pointer to struct Save with two singly linked list
- * @dt: separator
- * @end: string indicating how it will end
- * @c: flag reporting the number of folders
+ * @c: count the number of folders
+ * @o: 0 if it is a file, otherwise 1
  * @op: pointer to struc Option with printing options
+ * @val: 0 if it is the first printer, otherwise 1
  *
  * Return: Nothing
  */
@@ -30,41 +30,41 @@ int printer(save *safe, int c, char o, option *op, int val)
 	sort *tmp = NULL, *del = NULL;
 	int bol = 0;
 
-	if (val == 1 && o == 1 && c > 1 && op->detail != 2)
-		printf("\n");
 	while (safe)
 	{
-		if (safe->type != o)
-			safe = safe->next, continue;
-		if ((c > 1 && o == 1))
+		if (safe->type == o)
 		{
-			if (bol == 1 && op->detail < 2)
-				printf("\n");
-			if (op->detail == 2)
-				printf("\n");
-			printf("%s:\n", (safe->file));
-		}, tmp = safe->h;
-		while (tmp)
-		{
-			del = tmp->next;
-			if (type_hidden(op->hidden, tmp) == 1)
+			tmp = safe->h;
+			if (c > 1 && o == 1 && tmp)
 			{
+				if (bol == 1 && op->detail < 2)
+					printf("\n");
 				if (op->detail == 2)
-					more_detail(tmp, op);
-				printf("%s", (tmp)->r), val = 1;
-				if (o == 1 && del != NULL)
-					op->detail == 0 ? printf("  ") : printf("\n");
+					printf("\n");
+				printf("%s:\n", (safe->file));
 			}
-			(tmp) = ((tmp)->next);
+			while (tmp)
+			{
+				del = tmp->next;
+				if (type_hidden(op->hidden, tmp) == 1)
+				{
+					if (op->detail == 2)
+						more_detail(tmp, op);
+					printf("%s", (tmp)->r), val++;
+					if (o == 1 && del != NULL)
+						op->detail == 0 ? printf("  ") : printf("\n");
+				}
+				(tmp) = ((tmp)->next);
+			}
+			if ((op->detail >= 0 && o == 1 && safe->type == o)
+			|| (op->detail > 0 && o == 0 && safe->type == o))
+				printf("\n");
+			if (op->detail == 0 && o == 0 && safe->type == o)
+				printf("  ");
 		}
-		if ((op->detail >= 0 && o == 1 && safe->type == o) ||
-		    (op->detail > 0 && o == 0 && safe->type == o))
-			printf("\n");
-		if (op->detail == 0 && o == 0 && safe->type == o)
-			printf("  ");
 		safe = safe->next, bol = 1;
 	}
-	if (op->detail == 0 && o == 0 && val == 1)
+	if (op->detail == 0 && o == 0 && val > 0)
 		printf("\n");
 	return (val);
 }
