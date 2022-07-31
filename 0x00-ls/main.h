24a2a1fae730e3ae04dfc000568f1218f6a5594c
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-07-30 19:47:50 -0500

    Feat(New): Add new file and delete unused prototype main.h

--- 0x00-ls/main.h
+++ 0x00-ls/main.h
@@ -127,7 +127,6 @@ char *_strdup(char *str);
 void *_calloc(unsigned int nmemb, unsigned int size);
 
 save *sorting_save(save *head, save *node);
-save *reverse_save(save *head);
 save *sorting_save_reverse(save *head, save *node);
 
 /*
