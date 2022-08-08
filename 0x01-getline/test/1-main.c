78c1d860a5dd89234d24b6137b51d4a147d503ed
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-08-07 21:49:11 -0500

    Feat(Test): New tests were added test/1-main.c

--- 0x01-getline/test/1-main.c
+++ 0x01-getline/test/1-main.c
@@ -10,17 +10,34 @@
  *
  * Return: always 0.
  */
-int main(void)
+int main(int argc, char **argv)
 {
-    int fd = 0;
+    int fd = 0, i = 0;
     char *line = NULL;
+    (void) argc;
+    fd = open(argv[1], O_RDONLY);
 
-    fd = open("1-main.c", 0);
     while ((line = _getline(fd)))
     {
-        printf("%s\n", line);
+        printf("%s", line);
+        putchar(10);
         free(line);
+        if (i == 5)
+            break;
+        i++;
     }
+    i = 0;
+    while ((line = _getline(fd)))
+    {
+        printf("%s", line);
+        putchar(10);
+        free(line);
+        if (i == 5)
+            break;
+        i++;
+    }
+
     close(fd);
+    _getline(-1);
     return (0);
 }
