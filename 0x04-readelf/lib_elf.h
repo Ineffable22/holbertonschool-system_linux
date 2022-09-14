98f2e0b8a12c8c2adebcf7e7fd977e730196cb30
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-09-13 19:43:56 -0500

    Feat(Rework): Code reworked lib_elf.h

--- 0x04-readelf/lib_elf.h
+++ 0x04-readelf/lib_elf.h
@@ -7,11 +7,47 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+/* -----  ----- */
 
+
+
+
+/* ----- file_header_1.c ----- */
+void file_header_1(Elf64_Ehdr *header);
+void file_header_2(Elf64_Ehdr *header);
+void printer_file_header(char *key, char *value, char type);
 void check_elf(Elf64_Ehdr *header);
-void osabi(Elf64_Ehdr *header);
-void machine(Elf64_Ehdr *header);
-void version(Elf64_Ehdr *header);
-void number(Elf64_Ehdr *header);
+
+/* ----- file_header_2.c ----- */
+void magic_number(Elf64_Ehdr *header);
+char *class_file(Elf64_Ehdr *header);
+char *data(Elf64_Ehdr *header);
+char *version_0(Elf64_Ehdr *header);
+char *os_abi(Elf64_Ehdr *header);
+
+/* ----- file_header_3.c ----- */
+char *abi_version(Elf64_Ehdr *header);
+char *type(Elf64_Ehdr *header);
+char *machine(Elf64_Ehdr *header);
+char *version_1(Elf64_Ehdr *header);
+char *point_address(Elf64_Ehdr *header);
+
+/* ----- file_header_4.c ----- */
+char *start_program_headers(Elf64_Ehdr *header);
+char *start_section_headers(Elf64_Ehdr *header);
+char *flags(Elf64_Ehdr *header);
+char *header_size(Elf64_Ehdr *header);
+char *program_header_size(Elf64_Ehdr *header);
+
+/* ----- file_header_5.c ----- */
+char *program_headers_number(Elf64_Ehdr *header);
+char *section_headers_size(Elf64_Ehdr *header);
+char *section_headers_number(Elf64_Ehdr *header);
+char *section_headers_index(Elf64_Ehdr *header);
+
+/* ----- holbie_tools.c ----- */
+int _strlen(char *s);
+int _strcmp(char *s1, char *s2);
+char *itoa(int val, int base);
 
 #endif
