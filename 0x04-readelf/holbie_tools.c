77d913158645eca2ad4157813a220c12be25f857
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-09-13 19:43:50 -0500

    Feat(Rework): Code reworked holbie_tools.c

---
+++ 0x04-readelf/holbie_tools.c
@@ -0,0 +1,56 @@
+#include "lib_elf.h"
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: string to evaluate
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int x;
+
+	x = 0;
+	while (s[x] != '\0')
+		x++;
+	return (x);
+}
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first string to compare
+ * @s2: second string to compare
+ *
+ * Return: less than 0 if s1 is less than s2, 0 if they're equal,
+ * more than 0 if s1 is greater than s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 == *s2)
+	{
+		if (*s1 == 0)
+		{
+			return (0);
+		}
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
+
+/**
+ * itoa - Converts a number to a string
+ * @val: Number to convert
+ * @base: Number base
+ *
+ * Return: String with number
+ */
+char *itoa(int val, int base)
+{
+	static char buf[32] = {0};
+	int i = 30;
+
+	for (; val && i ; --i, val /= base)
+		buf[i] = "0123456789abcdef"[val % base];
+
+	return (&buf[i + 1]);
+}
