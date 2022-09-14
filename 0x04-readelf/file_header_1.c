7715d321d3f2672620ca0a59fd50d9e0c0f77f5d
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-09-13 19:53:14 -0500

    Feat(rename): Files renamed file_header_1.c

--- 0x04-readelf/file_header_1.c
+++ 0x04-readelf/file_header_1.c
@@ -1,163 +1,6 @@
 #include "lib_elf.h"
 
 /**
- * main - Displays the information contained in the ELF file header
- * of an ELF file.
- * @argc: Arguments number
- * @argv: Each argument per pointer
- *
- * Return: 0 if successful, else exit on error
- */
-int main(int argc, char **argv)
-{
-	Elf64_Ehdr *header = NULL, size;
-	int fd = 0, rd = 0;
-
-	if (argc != 2)
-	{
-		fprintf(stderr, "Usage: 0-hreadelf elf_filename\n");
-		exit(98);
-	}
-
-	header = malloc(sizeof(size));
-	if (header == NULL)
-	{
-		fprintf(stderr, "Can't malloc\n");
-		exit(98);
-	}
-	fd = open(argv[1], O_RDONLY);
-	if (fd == -1)
-	{
-		fprintf(stderr, "Can't open file %s\n", argv[1]);
-		exit(98);
-	}
-	rd = read(fd, header, sizeof(size));
-	if (rd == -1)
-	{
-		fprintf(stderr, "Can't read file %s\n", argv[1]);
-		exit(98);
-	}
-
-	file_header_1(header);
-	file_header_2(header);
-	free(header);
-	return (0);
-}
-
-/**
- * file_header_1 - Sends the function to validate
- * @header: Pointer to the ELF header
- *
- * Return: Nothing
- */
-void file_header_1(Elf64_Ehdr *header)
-{
-	char *str = NULL;
-
-	check_elf(header);
-
-	magic_number(header);
-
-	str = class_file(header);
-	printer_file_header("Class", str, 0);
-
-	str = data(header);
-	printer_file_header("Data", str, 0);
-
-	str = version_0(header);
-	printer_file_header("Version", str, 0);
-
-	str = os_abi(header);
-	printer_file_header("OS/ABI", str, 0);
-
-	str = abi_version(header);
-	printer_file_header("ABI Version", str, 0);
-
-	str = type(header);
-	printer_file_header("Type", str, 0);
-
-	str = machine(header);
-	printer_file_header("Machine", str, 0);
-
-	str = version_1(header);
-	printer_file_header("Version", str, 1);
-
-	str = point_address(header);
-	printer_file_header("Entry Point Address", str, 1);
-
-	str = start_program_headers(header);
-	printer_file_header("Start of program headers", str, 2);
-
-	str = start_section_headers(header);
-	printer_file_header("Start of section headers", str, 2);
-}
-
-/**
- * file_header_2 - Sends the function to validate
- * @header: Pointer to the ELF header
- *
- * Return: Nothing
- */
-void file_header_2(Elf64_Ehdr *header)
-{
-	char *str = NULL;
-
-	str = flags(header);
-	printer_file_header("Flags", str, 1);
-
-	str = header_size(header);
-	printer_file_header("Size of this header", str, 3);
-
-	str = program_header_size(header);
-	printer_file_header("Size of program headers", str, 3);
-
-	str = program_headers_number(header);
-	printer_file_header("Number of program headers", str, 0);
-
-	str = section_headers_size(header);
-	printer_file_header("Size of section headers", str, 3);
-
-	str = section_headers_number(header);
-	printer_file_header("Number of section headers", str, 0);
-
-	str = section_headers_index(header);
-	printer_file_header("Section header string table index", str, 0);
-}
-
-/**
- * printer_file_header - Prints in file header order format
- * @key: Key to insert
- * @value: Value to insert
- * @type: Type of format to add
- *
- * Return: Nothing
- */
-void printer_file_header(char *key, char *value, char type)
-{
-	int i = 0;
-
-	putchar(0x20);
-	putchar(0x20);
-	printf("%s", key);
-	putchar(0x3a);
-	for (i = 34 - _strlen(key); i > 0; i--)
-	{
-		putchar(0x20);
-	}
-	if (type == 1)
-	{
-		putchar(0x30);
-		putchar(0x78);
-	}
-	printf("%s", value);
-	if (type == 2)
-		printf(" (bytes into file)");
-	if (type == 3)
-		printf(" (bytes)");
-	putchar(0xa);
-}
-
-/**
  * check_elf - Checks if file is ELF file
  * @header: Pointer to the ELF header
  *
@@ -174,4 +17,68 @@ void check_elf(Elf64_Ehdr *header)
 		printf("magic bytes at the start\n");
 		exit(1);
 	}
+}
+
+/**
+ * magic_number - Prints the magic number
+ * @header: Pointer to the ELF header
+ *
+ * Return: Nothing
+ */
+void magic_number(Elf64_Ehdr *header)
+{
+	int i = 0;
+
+	printf("ELF Header:\n");
+	printf("  Magic:   ");
+	for (i = 0; i != EI_NIDENT; i++)
+		printf("%02x ", header->e_ident[i]);
+	putchar(0xa);
+}
+
+/**
+ * class_file - Checks the class of the file
+ * @header: Pointer to the ELF header
+ *
+ * Return: Resulting string
+ */
+char *class_file(Elf64_Ehdr *header)
+{
+	if (header->e_ident[EI_CLASS] == ELFCLASS32)
+		return ("ELF32");
+	else if (header->e_ident[EI_CLASS] == ELFCLASS64)
+		return ("ELF64");
+	else /* ELFCLASSNONE */
+		return ("invalid");
+}
+
+/**
+ * data - Checks the data of the file
+ * @header: Pointer to the ELF header
+ *
+ * Return: Resulting string
+ */
+char *data(Elf64_Ehdr *header)
+{
+	if (header->e_ident[EI_DATA] == ELFDATA2LSB)
+		return ("2's complement, little endian");
+
+	else if (header->e_ident[EI_DATA] == ELFDATA2MSB)
+		return ("2's complement, big endian");
+	else /* ELFDATANONE */
+		return ("Unknown data format");
+}
+
+/**
+ * version_0 - Checks the version of the file
+ * @header: Pointer to the ELF header
+ *
+ * Return: Resulting string
+ */
+char *version_0(Elf64_Ehdr *header)
+{
+	if (header->e_ident[EI_VERSION] == EV_CURRENT)
+		return ("1 (current)");
+	else /* (header->e_ident[EI_VERSION] == EV_NONE) */
+		return ("Invalid version");
 }
