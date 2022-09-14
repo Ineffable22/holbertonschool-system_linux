40c2b8998eff2c0caeb1768ac157053c6fb343a6
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-09-13 19:53:18 -0500

    Feat(rename): Files renamed file_header_3.c

--- 0x04-readelf/file_header_3.c
+++ 0x04-readelf/file_header_3.c
@@ -1,103 +1,6 @@
 #include "lib_elf.h"
 
 /**
- * abi_version - Checks the ABI version of the file
- * @header: Pointer to the ELF header
- *
- * Return: Resulting string
- */
-char *abi_version(Elf64_Ehdr *header)
-{
-	if (header->e_ident[EI_ABIVERSION] == 1)
-		return ("1");
-	else /* (header->e_ident[EI_ABIVERSION] == 0) */
-		return ("0");
-}
-
-/**
- * type - Checks the file type
- * @header: Pointer to the ELF header
- *
- * Return: Resulting string
- */
-char *type(Elf64_Ehdr *header)
-{
-	if (header->e_type == ET_REL)
-		return ("REL (A relocatable file)");
-	if (header->e_type == ET_EXEC)
-		return ("EXEC (An executable file)");
-	if (header->e_type == ET_DYN)
-		return ("DYN (Shared object file)");
-	if (header->e_type == ET_CORE)
-		return ("CORE (A core file)");
-	else /* (header->e_type == ET_NONE) */
-		return ("NONE (An unknown type)");
-}
-
-/**
- * machine - Identifies the type of machine
- * @header: Pointer to the ELF header
- *
- * Return: Nothing
- */
-char *machine(Elf64_Ehdr *header)
-{
-	if (header->e_machine == EM_M32)
-		return ("AT&T WE 32100");
-	if (header->e_machine == EM_SPARC)
-		return ("Sun Microsystems SPARC");
-	if (header->e_machine == EM_386)
-		return ("Intel 80386");
-	if (header->e_machine == EM_68K)
-		return ("Motorola 68000");
-	if (header->e_machine == EM_88K)
-		return ("Motorola 88000");
-	if (header->e_machine == EM_860)
-		return ("Intel 80860");
-	if (header->e_machine == EM_MIPS)
-		return ("MIPS RS3000 (big-endian only)");
-	if (header->e_machine == EM_PARISC)
-		return ("HP/PA");
-	if (header->e_machine == EM_SPARC32PLUS)
-		return ("SPARC with enhanced instruction set");
-	if (header->e_machine == EM_PPC)
-		return ("PowerPC");
-	if (header->e_machine == EM_PPC64)
-		return ("PowerPC 64-bit");
-	if (header->e_machine == EM_S390)
-		return ("IBM S/390");
-	if (header->e_machine == EM_ARM)
-		return ("Advanced RISC Machines");
-	if (header->e_machine == EM_SH)
-		return ("Renesas SuperH");
-	if (header->e_machine == EM_SPARCV9)
-		return ("SPARC v9 64-bit");
-	if (header->e_machine == EM_IA_64)
-		return ("Intel Itanium");
-	if (header->e_machine == EM_X86_64)
-		return ("Advanced Micro Devices x86-64");
-	if (header->e_machine == EM_VAX)
-		return ("DEC Vax");
-	else /* (header->e_machine == EM_NONE) */
-		return ("An unknown machine");
-}
-
-
-/**
- * version_1 - Identifies the version and address
- * @header: Pointer to the ELF header
- *
- * Return: Nothing
- */
-char *version_1(Elf64_Ehdr *header)
-{
-	if (header->e_version == EV_CURRENT)
-		return ("1");
-	else /* (header->e_version == EV_NONE) */
-		return ("0");
-}
-
-/**
  * point_address - Identifies the entry point address
  * @header: Pointer to the ELF header
  *
@@ -109,6 +12,62 @@ char *point_address(Elf64_Ehdr *header)
 	{
 		return (itoa(header->e_entry, 16));
 	}
+	else
+		return ("0");
+}
+
+/**
+ * start_program_headers - Identifies the start of program headers
+ * @header: Pointer to the ELF header
+ *
+ * Return: Resulting string
+ */
+char *start_program_headers(Elf64_Ehdr *header)
+{
+	if (header->e_phoff)
+		return (itoa(header->e_phoff, 10));
+	else
+		return ("0");
+}
+
+/**
+ * start_section_headers - Identifies the start of section headers
+ * @header: Pointer to the ELF header
+ *
+ * Return: Resulting string
+ */
+char *start_section_headers(Elf64_Ehdr *header)
+{
+	if (header->e_shoff)
+		return (itoa(header->e_shoff, 10));
+	else
+		return ("0");
+}
+
+/**
+ * flags - Identifies the flags
+ * @header: Pointer to the ELF header
+ *
+ * Return: Resulting string
+ */
+char *flags(Elf64_Ehdr *header)
+{
+	if (header->e_flags)
+		return (itoa(header->e_flags, 16));
+	else
+		return ("0");
+}
+
+/**
+ * header_size - Identifies the size of this header
+ * @header: Pointer to the ELF header
+ *
+ * Return: Resulting string
+ */
+char *header_size(Elf64_Ehdr *header)
+{
+	if (header->e_ehsize)
+		return (itoa(header->e_ehsize, 10));
 	else
 		return ("0");
 }
