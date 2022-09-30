ea48fab594559799dfb8ece4426c8149c9eba1b6
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-09-29 22:07:35 -0500

    Add: New file 10-strspn.asm

---
+++ 0x05-libasm/10-strspn.asm
@@ -0,0 +1,32 @@
+BITS 64
+        global  asm_strspn
+
+asm_strspn:
+        push    rbp
+        mov     rbp, rsp
+	mov	rax, 0x0
+	mov	rcx, rsi
+
+loop:
+	movzx	edx, byte[rdi]
+	cmp	dl, 0x0
+	je	end
+	cmp	byte [rsi], 0x0
+	je	end
+	cmp	dl, byte [rsi]
+	je	found
+	inc	rsi
+	jmp	loop
+
+found:
+	inc	rax
+
+not_found:
+	inc	rdi
+	mov	rsi, rcx
+	jmp	loop
+
+end:
+        mov     rsp, rbp
+        pop     rbp
+        ret
