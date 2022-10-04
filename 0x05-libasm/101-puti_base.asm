9238926e541a3ab7d64b0c24dc0d0bd75b1da97c
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-10-04 14:22:03 -0500

    Feat(Update): Update code and files directory 101-puti_base.asm

--- 0x05-libasm/101-puti_base.asm
+++ 0x05-libasm/101-puti_base.asm
@@ -3,7 +3,6 @@ BITS 64
 	extern	asm_putc
 	extern	asm_strlen
 
-
 asm_puti_base:
 	push	rbp
 	mov	rbp, rsp
@@ -31,95 +30,88 @@ asm_puti_base:
 after_negative:
 	mov	rdi, rsi
 	call	asm_strlen
-	mov	rbx, rax	; case 0?
-	cmp rbx, 1
-	jnz test_zero
-	; base is 1
-	mov r14, r15
-	loop_unary:
-	test r15, r15
-	jz done_loop_unary
-	xor rax, rax
-	mov al, BYTE [rsi]
-	push rax
-	dec r15
-	jmp loop_unary
-	done_loop_unary:
-	xor rax, rax
-	jmp while_n
+	mov	rbx, rax
+	cmp	rbx, 1
+	jnz	first
+	mov	r14, r15
 
-test_zero:
-	; handle case where n == 0
-	test r15, r15
-	jnz while_n
-	movzx rdi, BYTE [rsi]
-	call asm_putc
-	mov r13, 1
+loop_unary:
+	test	r15, r15
+	jz	done_loop_unary
+	xor	rax, rax
+	mov	al, BYTE [rsi]
+	push	rax
+	dec	r15
+	jmp	loop_unary
+
+done_loop_unary:
+	xor	rax, rax
+	jmp	while_n
+
+first:
+	test	r15, r15
+	jnz	while_n
+	movzx	rdi, BYTE [rsi]
+	call	asm_putc
+	mov	r13, 1
 
 while_n:
-	test rbx, rbx
-	jz while_n_nobase
-	; RAX starts as dividend becomes quotient
-	; RDX receives remainder
-	xor rdx, rdx
-	test r15, r15
-	jz done_while_n
-	mov rax, r15
-	;mov rbx, 10
-	div rbx
-	; print remainder in RDX
-	mov r15, rax
-	xor rax, rax
-	mov al, BYTE [rsi + rdx]
-	push rax
-	inc r14
-	jmp while_n
+	test	rbx, rbx
+	jz	while_n_nobase
+	xor	rdx, rdx
+	test	r15, r15
+	jz	done_while_n
+	mov	rax, r15
+	div	rbx
+	mov	r15, rax
+	xor	rax, rax
+	mov	al, BYTE [rsi + rdx]
+	push	rax
+	inc	r14
+	jmp	while_n
+
 done_while_n:
-	jmp done_both_while
+	jmp	other_loop
 
 while_n_nobase:
-	; RAX starts as dividend becomes quotient
-	; RDX receives remainder
-	xor rdx, rdx
-	test r15, r15
-	jz done_while_n_nobase
-	mov rax, r15
-	mov rbx, 10
-	div rbx
-	; print remainder in RDX
-	mov r15, rax
-	xor rax, rax
-	mov rax, rdx
-	add rax, 48
-	push rax
-	inc r14
-	jmp while_n_nobase
-done_while_n_nobase:
+	xor	rdx, rdx
+	test	r15, r15
+	jz	other_loop
+	mov	rax, r15
+	mov	rbx, 10
+	div	rbx
+	mov	r15, rax
+	xor	rax, rax
+	mov	rax, rdx
+	add	rax, 48
+	push	rax
+	inc	r14
+	jmp	while_n_nobase
 
-done_both_while:
-	mov r15, r14;	move counter
+other_loop:
+	mov	r15, r14
+
 print_loop:
-	test r15, r15
-	jz done_print_loop
-	pop rdi
-	after_pop:
-	;add rdi, 48
-	call asm_putc
-	dec r15
-	jmp print_loop
+	test	r15, r15
+	jz	done_print_loop
+	pop	rdi
+
+after_pop:
+	call	asm_putc
+	dec	r15
+	jmp	print_loop
 
 done_print_loop:
-
-	cmp r13, 1
-	jne end
-	inc r14
+	cmp	r13, 1
+	jne	end
+	inc	r14
 
 end:
-	mov rax, r14
-	pop r12
-	pop r13
-	pop r14
-	pop r15
-	mov rsp, rbp
-	pop rbp
+	mov	rax, r14
+	pop	r12
+	pop	r13
+	pop	r14
+	pop	r15
+	mov	rsp, rbp
+	pop	rbp
 	ret
