0ac195ac8d589be886c41a1ee1497c12c8da8d08
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-10-09 16:18:35 -0500

    Add: New file 100-all_in_one.c

---
+++ 0x06-signals/100-all_in_one.c
@@ -0,0 +1,36 @@
+#include "signals.h"
+
+/**
+ * handle_all_signal - Prints when signal is called
+ * @sig: The number of the signal that caused invocation of the handler.
+ * @info: A pointer to a siginfo_t, which is a structure containing
+ * further information about the signal, as described below.
+ * @ucontext: A pointer to a ucontext_t structure
+ *
+ * Return: Nothing
+ */
+void handle_all_signal(int sig, siginfo_t *info, void *ucontext)
+{
+	(void) sig;
+	(void) ucontext;
+	psiginfo(info, "Caught");
+}
+
+/**
+ * all_in_one - Sets up a single handler for all the signals
+ *
+ * Return: Nothing
+ **/
+void all_in_one(void)
+{
+	struct sigaction act;
+	int i = 0;
+
+	act.sa_sigaction = handle_all_signal;
+	for (i = 1; i < SIGRTMIN; i++)
+	{
+		if (i != SIGKILL && i != SIGSTOP)
+			if (sigaction(i, &act, NULL) == -1)
+				return;
+	}
+}
