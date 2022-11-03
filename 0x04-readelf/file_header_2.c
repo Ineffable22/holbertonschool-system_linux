00cce89a8daf660c8429c4badc5399f6005f72d1
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-11-03 11:12:08 -0500

    Feat(Update): code reworked file_header_2.c

--- 0x04-readelf/file_header_2.c
+++ 0x04-readelf/file_header_2.c
@@ -2,29 +2,29 @@
 
 /**
  * os_abi - Identifies the operating system
- * @header: Pointer to the ELF header
+ * @eh: Pointer to the ELF eh
  *
  * Return: Nothing
  */
-char *os_abi(Elf64_Ehdr *header)
+char *os_abi(elf eh)
 {
-	if (header->e_ident[EI_OSABI] == ELFOSABI_HPUX)
+	if (EGET(e_ident[EI_OSABI]) == ELFOSABI_HPUX)
 		return ("HP-UX");
-	if (header->e_ident[EI_OSABI] == ELFOSABI_NETBSD)
+	if (EGET(e_ident[EI_OSABI]) == ELFOSABI_NETBSD)
 		return ("NetBSD");
-	if (header->e_ident[EI_OSABI] == ELFOSABI_LINUX)
+	if (EGET(e_ident[EI_OSABI]) == ELFOSABI_LINUX)
 		return ("Linux");
-	if (header->e_ident[EI_OSABI] == ELFOSABI_SOLARIS)
+	if (EGET(e_ident[EI_OSABI]) == ELFOSABI_SOLARIS)
 		return ("Solaris");
-	if (header->e_ident[EI_OSABI] == ELFOSABI_IRIX)
+	if (EGET(e_ident[EI_OSABI]) == ELFOSABI_IRIX)
 		return ("IRIX");
-	if (header->e_ident[EI_OSABI] == ELFOSABI_FREEBSD)
+	if (EGET(e_ident[EI_OSABI]) == ELFOSABI_FREEBSD)
 		return ("FreeBSD");
-	if (header->e_ident[EI_OSABI] == ELFOSABI_TRU64)
+	if (EGET(e_ident[EI_OSABI]) == ELFOSABI_TRU64)
 		return ("TRU64 UNIX");
-	if (header->e_ident[EI_OSABI] == ELFOSABI_ARM)
+	if (EGET(e_ident[EI_OSABI]) == ELFOSABI_ARM)
 		return ("ARM architecture");
-	if (header->e_ident[EI_OSABI] == ELFOSABI_STANDALONE)
+	if (EGET(e_ident[EI_OSABI]) == ELFOSABI_STANDALONE)
 		return ("Stand-alone (embedded)");
 
 	/* EI_OSABI == ELFOSABI_NONE || ELFOSABI_SYSV */
@@ -33,96 +33,96 @@ char *os_abi(Elf64_Ehdr *header)
 
 /**
  * abi_version - Checks the ABI version of the file
- * @header: Pointer to the ELF header
+ * @eh: Pointer to the ELF eh
  *
  * Return: Resulting string
  */
-char *abi_version(Elf64_Ehdr *header)
+char *abi_version(elf eh)
 {
-	if (header->e_ident[EI_ABIVERSION] == 1)
+	if (EGET(e_ident[EI_ABIVERSION]) == 1)
 		return ("1");
-	else /* (header->e_ident[EI_ABIVERSION] == 0) */
+	else /* (EGET(e_ident[EI_ABIVERSION] == 0) */
 		return ("0");
 }
 
 /**
  * type - Checks the file type
- * @header: Pointer to the ELF header
+ * @eh: Pointer to the ELF eh
  *
  * Return: Resulting string
  */
-char *type(Elf64_Ehdr *header)
+char *type(elf eh)
 {
-	if (header->e_type == ET_REL)
+	if (EGET(e_type) == ET_REL)
 		return ("REL (A relocatable file)");
-	if (header->e_type == ET_EXEC)
+	if (EGET(e_type) == ET_EXEC)
 		return ("EXEC (An executable file)");
-	if (header->e_type == ET_DYN)
+	if (EGET(e_type) == ET_DYN)
 		return ("DYN (Shared object file)");
-	if (header->e_type == ET_CORE)
+	if (EGET(e_type) == ET_CORE)
 		return ("CORE (A core file)");
-	else /* (header->e_type == ET_NONE) */
+	else /* (EGET(e_type == ET_NONE) */
 		return ("NONE (An unknown type)");
 }
 
 /**
  * machine - Identifies the type of machine
- * @header: Pointer to the ELF header
+ * @eh: Pointer to the ELF eh
  *
  * Return: Nothing
  */
-char *machine(Elf64_Ehdr *header)
+char *machine(elf eh)
 {
-	if (header->e_machine == EM_M32)
+	if (EGET(e_machine) == EM_M32)
 		return ("AT&T WE 32100");
-	if (header->e_machine == EM_SPARC)
+	if (EGET(e_machine) == EM_SPARC)
 		return ("Sun Microsystems SPARC");
-	if (header->e_machine == EM_386)
+	if (EGET(e_machine) == EM_386)
 		return ("Intel 80386");
-	if (header->e_machine == EM_68K)
+	if (EGET(e_machine) == EM_68K)
 		return ("Motorola 68000");
-	if (header->e_machine == EM_88K)
+	if (EGET(e_machine) == EM_88K)
 		return ("Motorola 88000");
-	if (header->e_machine == EM_860)
+	if (EGET(e_machine) == EM_860)
 		return ("Intel 80860");
-	if (header->e_machine == EM_MIPS)
+	if (EGET(e_machine) == EM_MIPS)
 		return ("MIPS RS3000 (big-endian only)");
-	if (header->e_machine == EM_PARISC)
+	if (EGET(e_machine) == EM_PARISC)
 		return ("HP/PA");
-	if (header->e_machine == EM_SPARC32PLUS)
+	if (EGET(e_machine) == EM_SPARC32PLUS)
 		return ("SPARC with enhanced instruction set");
-	if (header->e_machine == EM_PPC)
+	if (EGET(e_machine) == EM_PPC)
 		return ("PowerPC");
-	if (header->e_machine == EM_PPC64)
+	if (EGET(e_machine) == EM_PPC64)
 		return ("PowerPC 64-bit");
-	if (header->e_machine == EM_S390)
+	if (EGET(e_machine) == EM_S390)
 		return ("IBM S/390");
-	if (header->e_machine == EM_ARM)
+	if (EGET(e_machine) == EM_ARM)
 		return ("Advanced RISC Machines");
-	if (header->e_machine == EM_SH)
+	if (EGET(e_machine) == EM_SH)
 		return ("Renesas SuperH");
-	if (header->e_machine == EM_SPARCV9)
+	if (EGET(e_machine) == EM_SPARCV9)
 		return ("SPARC v9 64-bit");
-	if (header->e_machine == EM_IA_64)
+	if (EGET(e_machine) == EM_IA_64)
 		return ("Intel Itanium");
-	if (header->e_machine == EM_X86_64)
+	if (EGET(e_machine) == EM_X86_64)
 		return ("Advanced Micro Devices x86-64");
-	if (header->e_machine == EM_VAX)
+	if (EGET(e_machine) == EM_VAX)
 		return ("DEC Vax");
-	else /* (header->e_machine == EM_NONE) */
+	else /* (EGET(e_machine == EM_NONE) */
 		return ("An unknown machine");
 }
 
 /**
  * version_1 - Identifies the version and address
- * @header: Pointer to the ELF header
+ * @eh: Pointer to the ELF eh
  *
  * Return: Nothing
  */
-char *version_1(Elf64_Ehdr *header)
+char *version_1(elf eh)
 {
-	if (header->e_version == EV_CURRENT)
+	if (EGET(e_version) == EV_CURRENT)
 		return ("1");
-	else /* (header->e_version == EV_NONE) */
+	else /* (EGET(e_version) == EV_NONE) */
 		return ("0");
 }
