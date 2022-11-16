37ea2096e5b4d88500ae8c56425c11dca4e67642
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-11-15 20:05:16 -0500

    Feat(Makefile): Update test/1-test_lists.py

--- 0x08_CPython/test/1-test_lists.py
+++ 0x08_CPython/test/1-test_lists.py
@@ -2,20 +2,20 @@ import ctypes
 
 lib = ctypes.CDLL('./libPyList.so')
 lib.print_python_list.argtypes = [ctypes.py_object]
-l = ['hello', 'World']
-lib.print_python_list(l)
-del l[1]
-lib.print_python_list(l)
-l = l + [4, 5, 6.0, (9, 8), [9, 8, 1024], "Holberton"]
-lib.print_python_list(l)
-l = []
-lib.print_python_list(l)
-l.append(0)
-lib.print_python_list(l)
-l.append(1)
-l.append(2)
-l.append(3)
-l.append(4)
-lib.print_python_list(l)
-l.pop()
-lib.print_python_list(l)
+lst = ['hello', 'World']
+lib.print_python_list(lst)
+del lst[1]
+lib.print_python_list(lst)
+lst = lst + [4, 5, 6.0, (9, 8), [9, 8, 1024], "Holberton"]
+lib.print_python_list(lst)
+lst = []
+lib.print_python_list(lst)
+lst.append(0)
+lib.print_python_list(lst)
+lst.append(1)
+lst.append(2)
+lst.append(3)
+lst.append(4)
+lib.print_python_list(lst)
+lst.pop()
+lib.print_python_list(lst)
