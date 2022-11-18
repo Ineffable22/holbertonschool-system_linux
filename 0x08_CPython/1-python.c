e3f63463ce974ab5299c3bcdd703fc0309bfd93a
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-11-17 21:44:13 -0500

    Feat(Task 1): Code reworked 1-python.c

--- 0x08_CPython/1-python.c
+++ 0x08_CPython/1-python.c
@@ -10,12 +10,12 @@
  */
 void print_python_list(PyObject *p)
 {
-	long int size = PyList_Size(p);
-	int i;
-	PyListObject *obj = (PyListObject *)p;
+	int i = 0;
 
-	printf("[*] Size of the Python List = %li\n", size);
-	printf("[*] Allocated = %li\n", obj->allocated);
-	for (i = 0; i < size; i++)
-		printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %lu\n", Py_SIZE(p));
+	printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
+	for (i = 0; i < Py_SIZE(p); i++)
+		printf("Element %d: %s\n", i, Py_TYPE(PyList_GetItem(p, i))
+		       ->tp_name);
 }
