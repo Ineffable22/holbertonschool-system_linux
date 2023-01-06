6c5b5a37881376bab55ccb4f4ee058eb744e6f55
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2023-01-06 15:29:24 -0500

    Add: New file multithreading.h

---
+++ 0x0A-multithreading/multithreading.h
@@ -0,0 +1,84 @@
+#ifndef _MULTITHREADING_H_
+#define _MULTITHREADING_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <stddef.h>
+#include <stdint.h>
+#define THREADS 7
+
+/**
+ * struct pixel_s - RGB pixel
+ *
+ * @r: Red component
+ * @g: Green component
+ * @b: Blue component
+ */
+typedef struct pixel_s
+{
+	uint8_t r;
+	uint8_t g;
+	uint8_t b;
+} pixel_t;
+
+/**
+ * struct img_s - Image
+ *
+ * @w:      Image width
+ * @h:      Image height
+ * @pixels: Array of pixels
+ */
+typedef struct img_s
+{
+	size_t w;
+	size_t h;
+	pixel_t *pixels;
+} img_t;
+
+/**
+ * struct kernel_s - Convolution kernel
+ *
+ * @size:   Size of the matrix (both width and height)
+ * @matrix: Kernel matrix
+ */
+typedef struct kernel_s
+{
+	size_t size;
+	float **matrix;
+} kernel_t;
+
+/**
+ * struct blur_portion_s - Information needed to blur a portion of an image
+ *
+ * @img:      Source image
+ * @img_blur: Destination image
+ * @x:        X position of the portion in the image
+ * @y:        Y position of the portion in the image
+ * @w:        Width of the portion
+ * @h:        Height of the portion
+ * @kernel:   Convolution kernel to use
+ */
+typedef struct blur_portion_s
+{
+	img_t const *img;
+	img_t *img_blur;
+	size_t x;
+	size_t y;
+	size_t w;
+	size_t h;
+	kernel_t const *kernel;
+} blur_portion_t;
+
+
+void *thread_entry(void *arg);
+int tprintf(char const *format, ...);
+void blur_portion(blur_portion_t const *portion);
+void insert_blur(const img_t *img, img_t *new_img, const kernel_t *kernel,
+		size_t x, size_t y);
+void blur_image(img_t *img_blur, img_t const *img, kernel_t const *kernel);
+void begin(void) __attribute__ ((constructor));
+void end(void) __attribute__ ((destructor));
+int tprintf(char const *format, ...);
+
+#endif /* _MULTITHREADING_H_ */
