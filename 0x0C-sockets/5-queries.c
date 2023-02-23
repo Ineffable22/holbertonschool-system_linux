cb48d68d790c13b645612d624930ee0a39a83a8b
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2023-02-23 00:33:06 -0500

    Add: New file 5-queries.c

---
+++ 0x0C-sockets/5-queries.c
@@ -0,0 +1,52 @@
+#include "socket.h"
+
+/**
+ * main - Opens an IPv4/TCP socket, and listens to traffic on port 8080.
+ * - Accept an entering connection
+ * - Print the IP address of the connected client
+ * - Wait for an incoming message from the connected client
+ * - Print the full received HTTP request
+ * - Print the break-down of the first line of the received HTTP request
+ * - Send back a response to the connected client (HTTP 200 OK)
+ * - Close the connection with the client
+ * - Wait for the next connection
+ *
+ * Return: Return: EXIT_SUCCESS if successful otherwise EXIT_FAILURE
+ */
+int main(void)
+{
+	return (start_server());
+}
+
+/**
+ * response - Print and send HTTP response
+ * @fd: file descriptor
+ * @buf: buffer to split and print
+ *
+ * Return: Always EXIT_SUCCESS
+ */
+int response(int fd, char *buf)
+{
+	char *allpath, *path, *queries;
+	char *delim1 = " \r\t\n";
+	char *delim2 = "&?";
+
+	strtok(buf, delim1);
+	allpath = strtok(NULL, delim1);
+	path = strtok(allpath, delim2);
+	printf("Path: %s\n", path);
+	while ((queries = strtok(NULL, delim2)))
+	{
+		printf("Query: \"");
+		for (; *queries != '='; queries++)
+			putchar(*queries);
+		printf("\" -> \"");
+		queries++;
+		for (; *queries; queries++)
+			putchar(*queries);
+		putchar(0x22);
+		putchar(0xA);
+	}
+	send(fd, "HTTP/1.1 200 OK\r\n\r\n", 17, 0);
+	return (EXIT_SUCCESS);
+}
