f00b670904900910270d80f564ed8c92d5b2ac8d
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2023-02-22 23:45:16 -0500

    Feat(Comment): Update information 4-request.c

--- 0x0C-sockets/4-request.c
+++ 0x0C-sockets/4-request.c
@@ -2,7 +2,15 @@
 
 /**
  * main - Opens an IPv4/TCP socket, and listens to traffic on port 8080
- *
+ * - Accept an entering connection
+ * - Print the IP address of the connected client
+ * - Wait for an incoming message from the connected client
+ * - Print the full received HTTP request
+ * - Print the break-down of the first line of the received HTTP request (See example)
+ * - Send back a response to the connected client (HTTP 200 OK)
+ * - Close the connection with the client
+ * - Wait for the next connection
+ * 
  * Return: Always EXIT_SUCCESS
 */
 int main(void)
